b0768f7d31242b1c432980fdc50710bbdda6c000
Ubuntu <ubuntu@ip-192-168-6-51.ec2.internal>
2018-06-12 12:15:36 +0000

    update script

--- infer_funny.sh
+++ infer_funny.sh
@@ -4,8 +4,8 @@ python3 infer.py \
 --drop_prob 0.8 \
 --path_to_train_data ../dataset/log_preprocess/train_binary.txt \
 --path_to_eval_data ../dataset/log_preprocess/test_binary.txt \
---save_path model_save_funny_binary/model.epoch_99 \
---predictions_path model_save_funny_binary/preds.txt
+--save_path model_save_funny_binary_adagrad/model.epoch_99 \
+--predictions_path model_save_funny_binary_adagrad/preds.txt
 
 #--path_to_train_data ../dataset/log_preprocess/train \
 #--path_to_eval_data ../dataset/log_preprocess/test \
--- train_funny.sh
+++ train_funny.sh
@@ -1,17 +1,17 @@
 python3 run.py --gpu_ids 0 \
 --path_to_train_data ../dataset/log_preprocess/train_binary.txt \
 --path_to_eval_data ../dataset/log_preprocess/test_binary.txt \
---logdir model_save_funny_binary \
+--logdir model_save_funny_binary_rmsprop \
 --hidden_layers 512,512,1024 \
 --non_linearity_type selu \
 --batch_size 128 \
 --drop_prob 0.8 \
---optimizer momentum \
+--optimizer rmsprop \
 --lr 0.005 \
 --weight_decay 0 \
 --aug_step 1 \
 --noise_prob 0 \
---num_epochs 400 \
+--num_epochs 100 \
 --summary_frequency 1000
 
 
